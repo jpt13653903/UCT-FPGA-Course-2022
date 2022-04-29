25dc402436010959588be02e824a288fa8202f3d
craques <cmuzhanye@gmail.com>
2022-04-29 03:22:42 -0700

    Added initial testbench

---
+++ Practicals/01 - Getting Started/Counter_TB.v
@@ -0,0 +1,24 @@
+`timescale 1ns/1ns
+
+module Counter_TB;
+	reg ipClk = 0;
+	@always #10 begin
+			ipClk <= ~ipClk;
+	end
+
+	reg ipReset = 1
+
+	initial begin
+			@(posedge ipClk);
+			@(posedge ipClk);
+			@(posedge ipClk);
+			ipReset <= 0;
+	end
+
+	wire [7:0] opLed;
+
+	Counter DUT(
+		.ipClk(ipClk)
+		.ipReset(ipReset)
+	)
+endmodule //Counter_TB

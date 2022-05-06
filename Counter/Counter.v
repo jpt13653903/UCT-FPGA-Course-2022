a8f3142895d10a5db2ec2c415ee173794960ed5a
John-Philip Taylor <John-PhilipT@reutech.co.za>
2022-05-06 14:19:48 +0200

    The UART is working in hardware

--- Counter/Counter.v
+++ Counter/Counter.v
@@ -21,6 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 module Counter(
   input       ipClk,
   input       ipnReset,
+
+  input       ipUART_Rx,
+  output      opUART_Tx,
+
   output [7:0]opLED
 );
 //------------------------------------------------------------------------------
@@ -34,6 +38,44 @@ always @(posedge ipClk) begin
 end
 
 assign opLED = ~Count[30:23];
+//------------------------------------------------------------------------------
+
+reg  [7:0]UART_TxData;
+reg       UART_TxSend;
+wire      UART_TxBusy;
+
+wire [7:0]UART_RxData;
+wire      UART_RxValid;
+
+UART UART_Inst(
+  .ipClk    (ipClk),
+  .ipReset  (Reset),
+
+  .ipTxData (  UART_TxData),
+  .ipTxSend (  UART_TxSend),
+  .opTxBusy (  UART_TxBusy),
+  .opTx     (opUART_Tx    ),
+
+  .ipRx     (ipUART_Rx     ),
+  .opRxData (  UART_RxData ),
+  .opRxValid(  UART_RxValid)
+);
+
+always @(posedge ipClk) begin
+  if(~UART_TxSend && ~UART_TxBusy) begin
+    case(UART_RxData) inside
+      8'h0D    : UART_TxData <= 8'h0A; // Change enter to linefeed
+      "0"      : UART_TxData <= 8'h0D; // Change 0 to carriage return
+      ["A":"Z"]: UART_TxData <= UART_RxData ^ 8'h20;
+      ["a":"z"]: UART_TxData <= UART_RxData ^ 8'h20;
+      default  : UART_TxData <= UART_RxData;
+    endcase
+    UART_TxSend <= UART_RxValid;
+
+  end else if(UART_TxSend && UART_TxBusy) begin
+    UART_TxSend <= 0;
+  end
+end
 //------------------------------------------------------------------------------
 
 endmodule

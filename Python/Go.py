a155ba01977964cc0a2a1a1a0e8e5b98011f1bfb
John-Philip Taylor <John-PhilipT@reutech.co.za>
2022-05-07 14:01:53 +0200

    New flow control working in hardware

--- Python/Go.py
+++ Python/Go.py
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 import audioread
 import serial
 import struct
+import time
+import sys
 #-------------------------------------------------------------------------------
 
 AudioFile = 'MyMusicFile.wav'
@@ -32,39 +34,85 @@ LEDs       = 0x02
 FIFO_Space = 0x03
 #-------------------------------------------------------------------------------
 
+def AudioGenerator(AudioFile):
+    with audioread.audio_open(AudioFile) as Audio:
+        print(Audio.channels, Audio.samplerate, Audio.duration)
+        sys.stdout.flush()
+        N = 0
+        for Buffer in Audio:
+            yield Buffer
+
+Buffer = AudioGenerator(AudioFile)
+#-------------------------------------------------------------------------------
+
 def Write(UART, Address, Data):
     UART.write(struct.pack('<BBBBBI', 0x55, 0x01, 0xAA, 0x05, Address, Data))
 #-------------------------------------------------------------------------------
 
 def Read(UART, Address):
     UART.write(struct.pack('<BBBBB', 0x55, 0x00, 0xAA, 0x01, Address))
-    return struct.unpack_from('<I', UART.read(9), offset=5)[0]
+
+def onReadResponse(Data):
+    Address = Data[0]
+    Data    = struct.unpack_from('<I', Data, offset=1)[0]
+    match Address:
+        case 0x03: # FIFO_Space
+            Write(UART, LEDs, 256-(Data >> 5))
+        case _:
+            pass
+    return True
 #-------------------------------------------------------------------------------
 
-def Stream(UART, Buffer):
-    Size = len(Buffer)
-    Sent = 0
+def onAudioFlowControl(UART, Data):
+    if(Data[0] == 4):
+        Space = 8192 # Timeout, so send a full queue
+        print('Received stream timeout')
+        sys.stdout.flush()
+    else:
+        Space = 2048 # Normal ack, so send one packet
 
-    Space = Read(UART, FIFO_Space)
-    Write(UART, LEDs, 256-(Space >> 5))
-    while(Space < Size): Space = Read(UART, FIFO_Space)
+    TotalSent = 0
+    while(TotalSent < Space):
+        try:
+            Packet = next(Buffer)
+        except StopIteration:
+            return False
 
-    while(Size > 0):
-        if(Size >= 256):
-            UART.write(struct.pack('<BBBB256B', 0x55, 0x10, 0xAA, 0x00, *Buffer[Sent:(Sent+256)]))
-            Size -= 256
-            Sent += 256
-        else:
-            UART.write(struct.pack(f'<BBBB{Size}B', 0x55, 0x10, 0xAA, 0x00, *Buffer[Sent:(Sent+Size)]))
-            Size  = 0
-            Sent += Size
+        Size = len(Packet)
+        Sent = 0
+
+        while(Size > 0):
+            if(Size >= 256):
+                UART.write(struct.pack('<BBBB256B', 0x55, 0x10, 0x10, 0x00, *Packet[Sent:(Sent+256)]))
+                Size -= 256
+                Sent += 256
+            else:
+                UART.write(struct.pack(f'<BBBB{Size}B', 0x55, 0x10, 0x10, 0x00, *Packet[Sent:(Sent+Size)]))
+                Size  = 0
+                Sent += Size
+        TotalSent += Sent
+    Read(UART, FIFO_Space)
+    return True
 #-------------------------------------------------------------------------------
 
-with audioread.audio_open(AudioFile) as Audio:
-    print(Audio.channels, Audio.samplerate, Audio.duration)
+def GetPacket(UART):
+    while(UART.read(1) != b'\x55'): pass
+    Header = UART.read(3)
+    Data   = UART.read(Header[2])
 
-    with serial.Serial(port='COM8', baudrate=3000000) as UART:
-        for Buffer in Audio: Stream(UART, Buffer)
-        Write(UART, LEDs, 0)
+    match Header[1]: # Source
+        case 0x00: # Read response
+            return onReadResponse(Data)
+        case 0x01: # Write response
+            return True
+        case 0x10: # Audio stream flow control
+            return onAudioFlowControl(UART, Data)
+        case _:
+            return True
+#-------------------------------------------------------------------------------
+
+with serial.Serial(port='COM8', baudrate=3000000) as UART:
+    while(GetPacket(UART)): pass
+    Write(UART, LEDs, 0)
 #-------------------------------------------------------------------------------
 

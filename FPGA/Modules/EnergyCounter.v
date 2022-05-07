69c434378c32892d11cfa1d6e1fbf64eda6e3f50
John-Philip Taylor <John-PhilipT@reutech.co.za>
2022-05-07 21:14:06 +0200

    Optimised EnergyCounter for size

--- FPGA/Modules/EnergyCounter.v
+++ FPGA/Modules/EnergyCounter.v
@@ -32,81 +32,86 @@ module EnergyCounter(
 );
 //------------------------------------------------------------------------------
 
-reg [30:0]N;
-reg [67:0]Sum;
-reg [23:0]AveragePower;
-reg [36:0]AveragePower_1;
-reg       AveragePower_Valid;
-
-always @(*) begin
-  case(ipWindowSize)
-    5'h00  : begin N = 31'h_0000_0000; AveragePower_1 = Sum[36: 0]; end
-    5'h01  : begin N = 31'h_0000_0001; AveragePower_1 = Sum[37: 1]; end
-    5'h02  : begin N = 31'h_0000_0003; AveragePower_1 = Sum[38: 2]; end
-    5'h03  : begin N = 31'h_0000_0007; AveragePower_1 = Sum[39: 3]; end
-    5'h04  : begin N = 31'h_0000_000F; AveragePower_1 = Sum[40: 4]; end
-    5'h05  : begin N = 31'h_0000_001F; AveragePower_1 = Sum[41: 5]; end
-    5'h06  : begin N = 31'h_0000_003F; AveragePower_1 = Sum[42: 6]; end
-    5'h07  : begin N = 31'h_0000_007F; AveragePower_1 = Sum[43: 7]; end
-    5'h08  : begin N = 31'h_0000_00FF; AveragePower_1 = Sum[44: 8]; end
-    5'h09  : begin N = 31'h_0000_01FF; AveragePower_1 = Sum[45: 9]; end
-    5'h0A  : begin N = 31'h_0000_03FF; AveragePower_1 = Sum[46:10]; end
-    5'h0B  : begin N = 31'h_0000_07FF; AveragePower_1 = Sum[47:11]; end
-    5'h0C  : begin N = 31'h_0000_0FFF; AveragePower_1 = Sum[48:12]; end
-    5'h0D  : begin N = 31'h_0000_1FFF; AveragePower_1 = Sum[49:13]; end
-    5'h0E  : begin N = 31'h_0000_3FFF; AveragePower_1 = Sum[50:14]; end
-    5'h0F  : begin N = 31'h_0000_7FFF; AveragePower_1 = Sum[51:15]; end
-    5'h10  : begin N = 31'h_0000_FFFF; AveragePower_1 = Sum[52:16]; end
-    5'h11  : begin N = 31'h_0001_FFFF; AveragePower_1 = Sum[53:17]; end
-    5'h12  : begin N = 31'h_0003_FFFF; AveragePower_1 = Sum[54:18]; end
-    5'h13  : begin N = 31'h_0007_FFFF; AveragePower_1 = Sum[55:19]; end
-    5'h14  : begin N = 31'h_000F_FFFF; AveragePower_1 = Sum[56:20]; end
-    5'h15  : begin N = 31'h_001F_FFFF; AveragePower_1 = Sum[57:21]; end
-    5'h16  : begin N = 31'h_003F_FFFF; AveragePower_1 = Sum[58:22]; end
-    5'h17  : begin N = 31'h_007F_FFFF; AveragePower_1 = Sum[59:23]; end
-    5'h18  : begin N = 31'h_00FF_FFFF; AveragePower_1 = Sum[60:24]; end
-    5'h19  : begin N = 31'h_01FF_FFFF; AveragePower_1 = Sum[61:25]; end
-    5'h1A  : begin N = 31'h_03FF_FFFF; AveragePower_1 = Sum[62:26]; end
-    5'h1B  : begin N = 31'h_07FF_FFFF; AveragePower_1 = Sum[63:27]; end
-    5'h1C  : begin N = 31'h_0FFF_FFFF; AveragePower_1 = Sum[64:28]; end
-    5'h1D  : begin N = 31'h_1FFF_FFFF; AveragePower_1 = Sum[65:29]; end
-    5'h1E  : begin N = 31'h_3FFF_FFFF; AveragePower_1 = Sum[66:30]; end
-    5'h1F  : begin N = 31'h_7FFF_FFFF; AveragePower_1 = Sum[67:31]; end
-    default: begin N = 31'h_7FFF_FFFF; AveragePower_1 = Sum[67:31]; end
-  endcase
-end
-//------------------------------------------------------------------------------
-
 reg       Reset;
 reg [31:0]Count;
+reg [ 4:0]ShiftCount;
 
-wire [35:0]Power_I = ipInput.I * ipInput.I;
-wire [35:0]Power_Q = ipInput.Q * ipInput.Q;
-wire [36:0]Power   = {1'b0,  Power_I} + {1'b0,  Power_Q};
+reg [30:0]N;
+reg [67:0]Sum;
+reg [23:0]Power;
+reg       Power_Valid;
+
+reg [35:0]Prod;
+
+COMPLEX_STREAM Input;
+
+enum { Idle, Sum_I, Sum_Q, Shift } State;
 
 always @(posedge ipClk) begin
   Reset <= ipReset;
 
   if(Reset) begin
-    Sum   <= 0;
-    Count <= 0;
+    N           <= 0;
+    Sum         <= 0;
+    Count       <= 0;
+    ShiftCount  <= 'hX;
 
-  end else if(ipInput.Valid) begin
-    if(Count >= N) begin
-      Sum   <= Power;
-      Count <= 0;
+    State       <= Idle;
+    Power_Valid <= 0;
+  //----------------------------------------------------------------------------
 
-      if(|AveragePower_1[36:34]) AveragePower <= ~24'd0;
-      else                       AveragePower <= AveragePower_1[33:10];
-      AveragePower_Valid <= 1;
-
-    end else begin
-      Sum   <= Sum   + Power;
-      Count <= Count + 1'b1;
-      AveragePower_Valid <= 0;
-    end
   end else begin
-    AveragePower_Valid <= 0;
+    case(State)
+      Idle: begin
+        Input       <= ipInput;
+        Prod        <= ipInput.I * ipInput.I;
+        Power_Valid <= 0;
+        ShiftCount  <= ipWindowSize;
+
+        if(ipInput.Valid) State <= Sum_I;
+      end
+      //------------------------------------------------------------------------
+
+      Sum_I: begin
+        Sum   <= Sum + Prod;
+        Prod  <= Input.Q * Input.Q;
+        State <= Sum_Q;
+      end
+      //------------------------------------------------------------------------
+
+      Sum_Q: begin
+        Sum <= Sum + Prod;
+
+        if(Count == N) begin
+          N     <= 0;
+          Count <= 0;
+          State <= Shift;
+
+        end else begin
+          Count <= Count + 1'b1;
+          State <= Idle;
+        end
+      end
+      //------------------------------------------------------------------------
+
+      Shift: begin
+        if(|ShiftCount) begin
+          N          <= {N[29:0], 1'b1};
+          Sum        <= {1'b0, Sum[67:1]};
+          ShiftCount <= ShiftCount - 1;
+
+        end else begin
+          if(|Sum[36:34]) Power <= ~24'd0;
+          else            Power <= Sum[33:10];
+          Power_Valid <= 1;
+          Sum         <= 0;
+          State       <= Idle;
+        end
+      end
+      //------------------------------------------------------------------------
+
+      default:;
+    endcase
   end
 end
 //------------------------------------------------------------------------------
@@ -118,8 +123,8 @@ LogScale LogScale_Inst(
   .ipClk  (ipClk  ),
   .ipReset(ipReset),
 
-  .ipInput(AveragePower),
-  .ipValid(AveragePower_Valid),
+  .ipInput(Power),
+  .ipValid(Power_Valid),
 
   .opOutput(Output),
   .opValid (OutputValid)

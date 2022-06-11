dc7f5906d4630272b41a1b0c70378abd49164cdc
craques <cmuzhanye@gmail.com>
2022-06-11 14:02:06 +0200

    Added logic to write to a register

--- Practicals/05 - Registers/Control/Control.v
+++ Practicals/05 - Registers/Control/Control.v
@@ -1,10 +1,15 @@
-module Control (
+module Control #(param DATA_LENGTH = 4, param BIT_WIDTH = 8) (
   input UART_PACKET ipTxPacket, 
 	output UART_PACKET opRxPacket, 
 	input ipReset,
-	input ipClk
+	input ipClk,
+	input ipWrEnable,
+	input wire [31:0] ipReadMemory,
+	output wire [31:0] opWriteMemory
 );
 
+
+	reg [4:0] dataLength;
 	UART_PACKET localTxPacket;
 	reg txLength;
 	reg [7:0] operationAddress;
@@ -50,25 +55,29 @@ module Control (
 				end
 			end
 			CONTROL_GET_OPERATION: begin
+				dataLength <= DATA_LENGTH;
 				if (localTxPacket.Valid && localTxPacket.Source == 0'x00 ) begin
 					controlState <= CONTROL_READ_DATA;
-				end else if(localTxPacket.Valid && localTxPacket.Source == 0'x00) begin
+				end else if(localTxPacket.Valid && localTxPacket.Source == 0'x01) begin
 					controlState <= CONTROL_WRITE_DATA;
 				end
 			end
 			CONTROL_READ_DATA: begin
 				//use address to write to this location
-				localRxData <= ipR
-				opRxData.
-				opRxData.Data <= localRxData
+				// during this state we want to send whatever data we have
+				if(dataLength != 0) begin
+					opRxPacket.Data <= ipReadMemory[(BIT_WIDTH*dataLength) - 1:BIT_WIDTH  * (dataLength - 1)]
+				end
 
 			end
 			CONTROL_WRITE_DATA: begin
 				//use address to read from this location
+				//during this state we want to store whatever data is incoming
 				localTxData <= ipTxPacket.Data
-				if(ipTxPacket.Valid) begin	
+				if(ipTxPacket.Valid && ipWrEnable) begin	// only enable write on valid and enable
+					// shift the bits in 
+					opWriteMemory <= { localTxData, opWriteMemory[31:8]};
 					if(txLength > 1 ) begin
-						localMemory <= { localTxData, localMemory[31:8]};
 						txLength <= txLength -1;
 					end else begin
 						controlState <= CONTROL_IDLE;
